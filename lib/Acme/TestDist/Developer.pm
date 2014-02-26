7f0a5fe65bef879bf9230edd8d21f85cce41aba6
Neil Bowers <neil@bowers.com>
2014-02-26 20:33:06 +0000

    Initial version

---
+++ lib/Acme/TestDist/Developer.pm
@@ -0,0 +1,34 @@
+package Acme::TestDist::Developer;
+
+use 5.006;
+use strict;
+use warnings;
+
+1;
+
+=head1 NAME
+
+Acme::TestDist::Developer - an example developer release
+
+=head1 SYNOPSIS
+
+ use Acme::TestDist::Developer;
+
+=head1 DESCRIPTION
+
+This is an empty distribution that's an example of a dist that
+has only ever been release as a developer release.
+
+=head1 AUTHOR
+
+Neil Bowers E<lt>neilb@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Neil Bowers <neilb@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
